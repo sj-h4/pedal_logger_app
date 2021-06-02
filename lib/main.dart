73c42981a05ecfaf5e8b8b4d88a3b7ea0b43274c
sj <sj@sjnoMac-mini.local>
2021-06-02 15:18:45 +0900

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -46,65 +31,136 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final String deviceName = "V3 BLE:0442838";
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+
+  BluetoothDevice targetDevice;
+  BluetoothCharacteristic bleCharactaristic;
+
+  final String serviceUUID = "00001818-0000-1000-8000-00805f9b34fb";
+  final String charactaristicUUID = "00002a63-0000-1000-8000-00805f9b34fb";
+
+  // デバイスと接続済みか
+  bool isConnected = false;
+
+  String deviceStatus = "No Device";
+
+  int power = 0;
+
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _startScan() {
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
+    log("deviceStatus: connecting");
+    if (isConnected) {
+      disconnectDevice();
+      return;
+    }
+
+    // Listen to scan results
+    flutterBlue.scanResults.listen((results) {
+      // do something with scan results
+      for (ScanResult r in results) {
+        print(r.device.name);
+        if (r.device.name == deviceName) {
+          isConnected = true;
+          targetDevice = r.device;
+          connectToDevice();
+          flutterBlue.stopScan();
+          break;
+        }
+      }
+    }, onDone: () => flutterBlue.stopScan());
+
+    // Stop scanning
+    flutterBlue.stopScan();
+  }
+
+  void connectToDevice() async {
+    if (targetDevice == null) return;
+
+    await targetDevice.connect();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      deviceStatus = "Connected: $targetDevice.name";
+      print('connected');
+    });
+    targetDevice.isDiscoveringServices.forEach((element) {
+      print("is discovering $element");
+    });
+    discoverServices();
+  }
+
+  void disconnectDevice() {
+    if (targetDevice == null) return;
+
+    targetDevice.disconnect();
+    isConnected = false;
+
+    setState(() {
+      deviceStatus = "disconnected";
+    });
+  }
+
+  void discoverServices() async {
+    if (targetDevice == null) return;
+    print("discovering");
+    targetDevice.isDiscoveringServices.forEach((element) {
+      print("is discovering $element");
+    });
+
+    List<BluetoothService> services = await targetDevice?.discoverServices();
+    services.forEach((element) {
+      print("service UUID: $element.uuid.toString");
+      if (element.uuid.toString() == serviceUUID) {
+        element.characteristics.forEach((charactaristic) {
+          print("cahractaristic UUID: $charactaristic.uuid.toString()");
+
+          if (charactaristic.uuid.toString() == charactaristicUUID) {
+            bleCharactaristic = charactaristic;
+            recieveNotification();
+          } else {
+            print("cannot");
+          }
+
+          print("connected service");
+        });
+      }
+    });
+  }
+
+  void recieveNotification() async {
+    if (targetDevice == null) return;
+    await bleCharactaristic?.setNotifyValue(true);
+
+    bleCharactaristic?.value?.listen((value) async {
+      setState(() {
+        power = value[3] * 256 + value[2];
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              '$power W',
+              style: Theme.of(context).textTheme.headline2,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _startScan,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
