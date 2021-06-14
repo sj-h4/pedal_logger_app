ae68e6435a4975a14cde440d2a1d1e4a73feb1df
sj <sakajoharuki@gmail.com>
2021-06-14 10:39:24 +0900

    a

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:pedal_logger_flutter/view/dataView.dart';
 
-import 'view/dataView.dart';
-import 'view/bottom_navi_view.dart';
-
 //https://github.com/kazuma-fujita/flutter_bottom_navigation_bar/blob/master/lib/main.dart
 
 final tabTypeProvider =
